503260c3aef08a994a55a159df6ef5414e7a618b
dvirtz <dvirtz@gmail.com>
2024-07-25 22:12:23 +0100

    feat: add venue support
    the script now links the venue if it finds it in MusicBrainz and adds the ability to add a venue as a MusicBrainz place.

--- scripts/setlistfm-musicbrainz-import/setlistfm-musicbrainz-import.user.js
+++ scripts/setlistfm-musicbrainz-import/setlistfm-musicbrainz-import.user.js
@@ -9,7 +9,8 @@
 // @downloadURL  https://github.com/dvirtz/musicbrainz-scripts/raw/main/scripts/setlistfm-musicbrainz-import/setlistfm-musicbrainz-import.user.js
 // @updateURL    https://github.com/dvirtz/musicbrainz-scripts/raw/main/scripts/setlistfm-musicbrainz-import/setlistfm-musicbrainz-import.user.js
 // @supportURL   https://github.com/dvirtz/musicbrainz-scripts/issues
-// @match        *://www.setlist.fm/setlist*
+// @match        *://www.setlist.fm/setlist/*
+// @match        *://www.setlist.fm/venue/*
 // @icon         https://api.setlist.fm/favicon.ico
 // @license      MIT
 // @run-at       document-end
@@ -27,9 +28,72 @@
 //         }
 //     }
 // }
+
+const Constants = {
+  Place: {
+    SetlistFmUrl: '817',
+  },
+  Event: {
+    SetlistFmUrl: '811',
+    MainPerformerGUID: '936c7c95-3156-3889-a062-8a0cd57f8946',
+    HeldAtGUID: 'e2c6f697-07dc-38b1-be0b-83d740165532',
+  },
+};
+
 main();
 
-function submitEvent() {
+function submitPlace() {
+  let searchParams = new URLSearchParams();
+
+  searchParams.append('edit-place.name', unsafeWindow.sfmPageAttributes.venue.name);
+
+  searchParams.append(
+    'edit-place.edit_note',
+    `Imported from ${document.location.href} using ${GM.info.script.name} version ${GM.info.script.version} from ${GM.info.script.namespace}.`
+  );
+
+  searchParams.append('edit-place.area.name', unsafeWindow.sfmPageAttributes.venue.city);
+
+  const infoPart = document.querySelector('div.info');
+  if (infoPart) {
+    for (const form of infoPart.querySelectorAll('.form-group')) {
+      const label = form.querySelector('.label').textContent;
+      switch (label) {
+        case 'Address':
+          searchParams.append(
+            'edit-place.address',
+            form.querySelector('span.address').innerText.replaceAll('\n', ', ')
+          );
+          break;
+        case 'Opened': {
+          const openedText = form.querySelector('span:not(.label)').textContent;
+          const tokens = openedText.split(' ');
+          searchParams.append('edit-place.period.begin_date.year', tokens[tokens.length - 1]);
+          if (tokens.length > 1) {
+            searchParams.append('edit-place.period.begin_date.month', convertMonth(tokens[tokens.length - 2]));
+            if (tokens.length > 2) {
+              searchParams.append('edit-place.period.begin_date.day', tokens[tokens.length - 3]);
+            }
+          }
+          break;
+        }
+        case 'Web':
+          form.querySelectorAll('span:not(.label) a').forEach((link, index) => {
+            searchParams.append(`edit-place.url.${index + 1}.text`, link.href);
+          });
+          break;
+      }
+    }
+  }
+
+  searchParams.append('edit-place.url.0.text', document.location.href);
+  searchParams.append('edit-place.url.0.link_type_id', Constants.Place.SetlistFmUrl);
+
+  // navigate to the place creation page
+  unsafeWindow.open('https://musicbrainz.org/place/create?' + searchParams.toString());
+}
+
+function submitEvent(placeMBID) {
   let searchParams = new URLSearchParams();
 
   const artistMBID = unsafeWindow.sfmPageAttributes.artist.mbid;
@@ -50,11 +114,7 @@ function submitEvent() {
 
   // setlist
   const setlist = [artist(artistName, artistMBID)]
-    .concat(
-      Array.from(document.querySelectorAll('.setlistParts').values()).flatMap(part => [
-        ...setlistEntry(part, artistName),
-      ])
-    )
+    .concat(Array.from(document.querySelectorAll('.setlistParts')).flatMap(part => [...setlistEntry(part, artistName)]))
     .join('\n');
   searchParams.append('edit-event.setlist', setlist);
 
@@ -87,12 +147,14 @@ function submitEvent() {
   );
 
   searchParams.append('edit-event.url.0.text', document.location.href);
-  searchParams.append('edit-event.url.0.link_type_id', '811');
+  searchParams.append('edit-event.url.0.link_type_id', Constants.Event.SetlistFmUrl);
 
-  const mainPerformerGUID = '936c7c95-3156-3889-a062-8a0cd57f8946';
-  searchParams.append('rels.0.type', mainPerformerGUID);
+  searchParams.append('rels.0.type', Constants.Event.MainPerformerGUID);
   searchParams.append('rels.0.target', artistMBID);
   searchParams.append('rels.0.direction', 'backward');
+
+  searchParams.append('rels.1.type', Constants.Event.heldAtGUID);
+  searchParams.append('rels.1.target', placeMBID);
 
   // navigate to the event creation page
   unsafeWindow.open('https://musicbrainz.org/event/create?' + searchParams.toString());
@@ -102,60 +164,38 @@ function submitEvent() {
 
 // add the button to the page
 async function main() {
-  let div = document.createElement('div');
-  div.classList.add('btn-group');
-
-  const addBtnElem = document.createElement('button');
-  addBtnElem.classList.add('btn');
-
-  const buttonIcon = document.createElement('img');
-  buttonIcon.src = 'https://musicbrainz.org/static/images/favicons/favicon-32x32.png';
-  buttonIcon.alt = 'MB';
-  buttonIcon.style.width = '16px';
-  buttonIcon.style.height = '16px';
-  buttonIcon.style.margin = '2px';
-  addBtnElem.appendChild(buttonIcon);
-
-  const buttonSpan = document.createElement('span');
-  addBtnElem.appendChild(buttonSpan);
-
-  div.appendChild(addBtnElem);
-
-  const userFragment = document.querySelector('.user-fragment');
-  userFragment.insertBefore(div, userFragment.firstChild);
-
-  const existingEvent = await fetch(
-    `https://musicbrainz.org/ws/2/url?resource=${document.location.href}&inc=event-rels&fmt=json`
-  );
-  if (existingEvent.ok) {
-    const body = await existingEvent.json();
-    const eventId = body['relations'][0]['event'].id;
-    buttonSpan.textContent = 'Open in MB';
-    addBtnElem.addEventListener('click', () => {
-      window.open(`https://musicbrainz.org/event/${eventId}`);
-    });
+  if (location.href.includes('/venue/')) {
+    await handleVenuePage();
   } else {
-    buttonSpan.textContent = 'Add to MB';
-    addBtnElem.addEventListener('click', () => {
-      submitEvent();
-    });
+    await handleSetlistPage();
   }
 }
 
 function convertMonth(monthName) {
   const monthMap = {
     Jan: 1,
+    January: 1,
     Feb: 2,
+    February: 2,
     Mar: 3,
+    March: 3,
     Apr: 4,
+    April: 4,
     May: 5,
     Jun: 6,
+    June: 6,
     Jul: 7,
+    July: 7,
     Aug: 8,
+    August: 8,
     Sep: 9,
+    September: 9,
     Oct: 10,
+    October: 10,
     Nov: 11,
+    November: 11,
     Dec: 12,
+    December: 12,
   };
 
   // convert 3-letter month name to number
@@ -209,5 +249,113 @@ function* setlistEntry(setlistPart, mainArtistName) {
     }
   } else if (setlistPart.classList.contains('encore') || setlistPart.classList.contains('section')) {
     yield `\n${info(setlistPart.textContent.trim())}`;
+  }
+}
+
+async function addWarningIcon(type, query, afterElement) {
+  const warningIcon = document.createElement('img');
+  warningIcon.src = 'https://musicbrainz.org/static/images/icons/warning.png';
+  warningIcon.alt = 'warning';
+  warningIcon.style.width = '16px';
+  warningIcon.style.height = '16px';
+  warningIcon.style.margin = '2px';
+
+  warningIcon.title = `${type} not found on MusicBrainz, click to search`;
+  warningIcon.addEventListener('click', () => {
+    window.open(`https://musicbrainz.org/search?query=${query}&type=${type}&method=advanced`);
+  });
+
+  afterElement.parentNode.insertBefore(warningIcon, afterElement.nextSibling);
+}
+
+async function findVenue(url) {
+  const existingVenue = await tryFetch(`https://musicbrainz.org/ws/2/url?resource=${url}&inc=place-rels&fmt=json`);
+  return existingVenue && existingVenue['relations'][0]['place'].id;
+}
+
+async function createAddButton() {
+  let div = document.createElement('div');
+  div.classList.add('btn-group');
+
+  const addBtnElem = document.createElement('button');
+  addBtnElem.classList.add('btn');
+
+  const buttonIcon = document.createElement('img');
+  buttonIcon.src = 'https://musicbrainz.org/static/images/favicons/favicon-32x32.png';
+  buttonIcon.alt = 'MB';
+  buttonIcon.style.width = '16px';
+  buttonIcon.style.height = '16px';
+  buttonIcon.style.margin = '2px';
+  addBtnElem.appendChild(buttonIcon);
+
+  const buttonSpan = document.createElement('span');
+  addBtnElem.appendChild(buttonSpan);
+
+  div.appendChild(addBtnElem);
+
+  const userFragment = document.querySelector('.user-fragment');
+  userFragment.insertBefore(div, userFragment.firstChild);
+  return {addBtnElem, buttonSpan};
+}
+
+async function findEvent(url) {
+  const existingEvent = await tryFetch(`https://musicbrainz.org/ws/2/url?resource=${url}&inc=event-rels&fmt=json`);
+  return existingEvent && existingEvent['relations'][0]['event'].id;
+}
+
+async function handleSetlistPage() {
+  const {addBtnElem, buttonSpan} = await createAddButton();
+
+  const eventMBID = await findEvent(document.location.href);
+  if (eventMBID) {
+    buttonSpan.textContent = 'Open in MB';
+    addBtnElem.addEventListener('click', () => {
+      window.open(`https://musicbrainz.org/event/${eventMBID}`);
+    });
+  } else {
+    const venueElement = document.querySelector('a[href*="/venue/"]');
+
+    const placeMBID = await findVenue(venueElement.href);
+    if (!placeMBID) {
+      addWarningIcon(
+        'place',
+        `place:${unsafeWindow.sfmPageAttributes.venue.name} AND area:${unsafeWindow.sfmPageAttributes.venue.city}`,
+        venueElement
+      );
+    }
+    buttonSpan.textContent = 'Add to MB';
+    addBtnElem.addEventListener('click', () => {
+      submitEvent(placeMBID);
+    });
+  }
+}
+
+async function handleVenuePage() {
+  const {addBtnElem, buttonSpan} = await createAddButton();
+
+  const placeMBID = await findVenue(document.location.href);
+  if (placeMBID) {
+    buttonSpan.textContent = 'Open in MB';
+    addBtnElem.addEventListener('click', () => {
+      window.open(`https://musicbrainz.org/place/${placeMBID}`);
+    });
+  } else {
+    buttonSpan.textContent = 'Add to MB';
+    addBtnElem.addEventListener('click', () => {
+      submitPlace();
+    });
+  }
+}
+
+async function tryFetch(url) {
+  try {
+    const result = await fetch(url, {headers: {'Accept': 'application/json'}});
+    if (!result.ok) {
+      throw new Error(`HTTP error: ${result.status}`);
+    }
+    return await result.json();
+  } catch (e) {
+    console.error(`Failed to fetch ${url}: ${e}`);
+    return null;
   }
 }
