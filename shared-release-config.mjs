6a2ff975c2468c6e84d49e8f6c46454dcd9e1d5a
dvirtz <dvirtz@gmail.com>
2024-12-02 21:04:50 +0000

    chore: create github release only from root
    the plugins release pipelines will only update the version and changelog

---
+++ shared-release-config.mjs
@@ -0,0 +1,51 @@
+const types = [
+  {type: 'feat', section: 'Features'},
+  {type: 'fix', section: 'Bug Fixes'},
+  {type: 'docs', section: 'Documentation'},
+  {type: 'perf', section: 'Performance improvements'},
+  {type: 'revert', section: 'Bug Fixes'},
+  {type: 'test', section: 'Tests'},
+  {type: 'ci', section: 'Build and continuous integration'},
+  {type: 'build', section: 'Build and continuous integration'},
+  {type: 'chore', section: 'General maintenance'},
+  {type: 'style', section: 'Style improvements'},
+  {type: 'refactor', section: 'Refactoring'},
+];
+
+const releaseRules = [
+  {type: 'chore', release: 'patch'},
+  {type: 'docs', release: 'patch'},
+  {type: 'revert', release: 'patch'},
+  {type: 'test', release: 'patch'},
+];
+
+export default {
+  branches: [
+    'main',
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalCommits',
+        presetConfig: {
+          types,
+          releaseRules,
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalCommits',
+        presetConfig: {
+          types,
+        },
+      },
+    ],
+  ],
+};
