615449627fe5ff33220bb205db1fb901317c9253
dvirtz <dvirtz@gmail.com>
2025-01-30 13:54:25 +0000

    fix: call exception catching fetch functions

--- src/common/musicbrainz/fetch.ts
+++ src/common/musicbrainz/fetch.ts
@@ -3,11 +3,11 @@ import {rateLimited} from 'src/common/lib/rate-limited';
 import {RATE_LIMIT_INTERVAL} from './constants';
 
 export async function tryFetchJSON<T>(url: string, options?: RequestInit) {
-  return await rateLimited(RATE_LIMIT_INTERVAL, () => fetch.fetchJSON<T>(url, options));
+  return await rateLimited(RATE_LIMIT_INTERVAL, () => fetch.tryFetchJSON<T>(url, options));
 }
 
 export async function tryFetchText(url: string, options?: RequestInit) {
-  return await rateLimited(RATE_LIMIT_INTERVAL, () => fetch.fetchText(url, options));
+  return await rateLimited(RATE_LIMIT_INTERVAL, () => fetch.tryFetchText(url, options));
 }
 
 export async function fetchJSON<T>(url: string, options?: RequestInit) {
