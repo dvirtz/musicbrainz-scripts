515e3788fc89b5419ea8652335a5555d96fc9fba
dvirtz <dvirtz@gmail.com>
2025-03-30 16:18:09 +0100

    ci: exponentially backoff tests

--- lib/test-support/src/userscript-test.ts
+++ lib/test-support/src/userscript-test.ts
@@ -2,9 +2,12 @@ import {test as base} from '@playwright/test';
 import path from 'path';
 import {UserscriptPage} from './userscript-page';
 
+const ONE_MINUTE = 60_000;
+const MAXIMUM_DELAY = 5 * ONE_MINUTE;
+
 export const test = base.extend<{userscriptPage: UserscriptPage}>({
   userscriptPage: [
-    async ({page}, use) => {
+    async ({page}, use, testInfo) => {
       const userscriptPage = new UserscriptPage(page);
 
       await userscriptPage.mockWindowOpen();
@@ -18,6 +21,15 @@ export const test = base.extend<{userscriptPage: UserscriptPage}>({
       });
 
       await use(userscriptPage);
+
+      // exponential backoff for flaky tests
+      // https://github.com/microsoft/playwright/issues/28857#issuecomment-2511850509
+      if (testInfo.status === 'failed') {
+        const delay = Math.min((testInfo.retry + 1) * ONE_MINUTE, MAXIMUM_DELAY);
+        testInfo.setTimeout(testInfo.timeout + delay);
+        console.info(`Exponential backoff: waiting ${delay}ms before the next test retry`);
+        await page.waitForTimeout(delay);
+      }
     },
     {auto: true},
   ],
