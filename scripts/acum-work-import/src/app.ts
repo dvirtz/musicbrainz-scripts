cb6e232869a4385032f34a4bf5d347e136d19f4b
dvirtz <dvirtz@gmail.com>
2025-04-06 00:22:18 +0100

    fix: wait for MB to be populated
    avoid using MB.entity as it's not loaded since https://github.com/metabrainz/musicbrainz-server/pull/3457 went in

--- scripts/acum-work-import/src/app.ts
+++ scripts/acum-work-import/src/app.ts
@@ -4,20 +4,40 @@ import {createWorkEditorUI} from './ui/work-editor-ui';
 
 main().catch(console.error);
 
+function waitForEditorState() {
+  return new Promise(resolve => {
+    const interval = setInterval(() => {
+      if (MB.relationshipEditor.state) {
+        console.debug('Editor state is ready');
+        clearInterval(interval);
+        resolve(MB.relationshipEditor.state);
+      }
+    }, 50); // Check every 50ms
+  });
+}
+
 async function main() {
+  console.debug('start');
   await registerSettingsDialog();
-  new MutationObserver((_mutations, observer) => {
+  await waitForEditorState();
+  const createUI = (_mutations: MutationRecord[], observer: MutationObserver) => {
     if (location.pathname.startsWith('/release/')) {
       if (document.querySelector('button.submit')) {
+        console.debug('Creating release editor');
         createReleaseEditorUI();
         observer.disconnect();
       }
     } else {
       const workForm = document.querySelector<HTMLFormElement>('form.edit-work');
       if (workForm) {
+        console.debug('Creating work editor');
         createWorkEditorUI(workForm);
         observer.disconnect();
       }
     }
-  }).observe(document.body, {childList: true, subtree: true});
+  };
+  const observer = new MutationObserver(createUI);
+  observer.observe(document.body, {childList: true, subtree: true});
+  // Initial call to createEditor to handle the case where the editor is already present
+  createUI([], observer);
 }
