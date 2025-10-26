04ded31770be9a49b375534d517d973bde104de8
dvirtz <dvirtz@gmail.com>
2025-10-26 22:46:34 +0000

    test: reuse test release
    avoid submitting edits so release can be reused

--- scripts/acum-work-import/tests/release-editor.spec.ts
+++ scripts/acum-work-import/tests/release-editor.spec.ts
@@ -5,7 +5,7 @@ import {expect, mergeTests} from '@playwright/test';
 const test = mergeTests(testRelease, musicbrainzPage);
 
 test.describe('release editor', () => {
-  test('can import album', async ({page, testRelease, musicbrainzPage}) => {
+  test('can import album', async ({page, testRelease, musicbrainzPage, baseURL}) => {
     await testRelease.editRelationships(musicbrainzPage);
 
     // fill in the album ID
@@ -26,9 +26,57 @@ test.describe('release editor', () => {
     const arrangerLabels = page.getByText('arranger:');
     await expect(arrangerLabels).toHaveCount(testRelease.tracks().length);
 
+    await page.route('**/work/create', async (route, request) => {
+      const postData = request.postDataJSON() as Record<string, string>;
+      expect(postData).toHaveProperty('edit-work.name');
+      const work = testRelease.work(postData['edit-work.name']!);
+      expect(work).toBeDefined();
+      expect(postData).toMatchObject({
+        'edit-work.name': work!.title,
+        'edit-work.comment': work!.disambiguation,
+        'edit-work.type_id': work!['type-id'],
+        ...work!.languages.reduce(
+          (acc, lang, index) => {
+            {
+              acc[`edit-work.language.${index}`] = lang;
+              return acc;
+            }
+          },
+          {} as Record<string, unknown>
+        ),
+        ...work!.iswcs.reduce(
+          (acc, iswc, index) => {
+            {
+              acc[`edit-work.iswc.${index}`] = iswc;
+              return acc;
+            }
+          },
+          {} as Record<string, unknown>
+        ),
+        ...work!.attributes.reduce(
+          (acc, attr, index) => {
+            {
+              acc[`edit-work.attribute.${index}.0`] = attr['type-id'];
+              acc[`edit-work.attribute.value.${index}.0`] = attr.value;
+              return acc;
+            }
+          },
+          {} as Record<string, unknown>
+        ),
+      });
+
+      await route.fulfill({
+        response: await page.request.get(`${baseURL}/work/${work!.id}`),
+      });
+    });
+
+    await page.route('**/edit/create', route => route.fulfill({json: {edits: []}}));
+
     // Now click "Enter edit" which will automatically submit works first, then proceed with the edit
     const enterEdit = page.getByRole('button', {name: 'Enter edit'});
     await enterEdit.click();
     await expect(page).toHaveURL(`/release/${testRelease.gid}`);
+
+    await page.unrouteAll();
   });
 });
