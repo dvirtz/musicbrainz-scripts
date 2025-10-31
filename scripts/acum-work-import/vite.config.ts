2b13ccd604b1ccc8b320b7faf97f81ad131347a3
dvirtz <dvirtz@gmail.com>
2025-10-31 11:52:10 +0000

    fix: main website match
    when switching from beta to main, ?unset_beta=1 is added to the URL

--- scripts/acum-work-import/vite.config.ts
+++ scripts/acum-work-import/vite.config.ts
@@ -6,8 +6,9 @@ export default defineConfig('acum-work-import', {
   version: '1.19.0',
   icon: 'https://nocs.acum.org.il/acumsitesearchdb/resources/images/faviconSite.svg',
   match: [
-    'http*://*.musicbrainz.org/release/*/edit-relationships',
-    'http*://*.musicbrainz.org/work/*',
+    'http*://*.musicbrainz.org/release/*/edit-relationships?*',
+    'http*://*.musicbrainz.org/work/*/edit?*',
+    'http*://*.musicbrainz.org/work/create?*',
     'http*://*.musicbrainz.org/dialog?path=%2Fwork%2F*',
   ],
   'run-at': 'document-end',
