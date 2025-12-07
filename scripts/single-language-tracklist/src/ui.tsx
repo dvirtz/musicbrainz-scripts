ef19eaf2eedbb6e57b47906ae536b4c42393df42
dvirtz <dvirtz@gmail.com>
2025-12-07 01:09:16 +0000

    feat(single-language-tracklist): custom separator
    closes #148

--- scripts/single-language-tracklist/src/ui.tsx
+++ scripts/single-language-tracklist/src/ui.tsx
@@ -6,29 +6,43 @@ import {TextField} from '@kobalte/core/text-field';
 import {ToolLine} from '@repo/common-ui/tool-line';
 import {toolbox} from '@repo/common-ui/toolbox';
 import {waitForElement} from '@repo/rxjs-ext/wait-for-element';
+import {createSignal} from 'solid-js';
 import {render} from 'solid-js/web';
 
-function SingleLanguageTracklistUI() {
+function SingleLanguageTracklistUI(props: {separator: string}) {
+  const [separator, setSeparator] = createSignal(props.separator);
+
+  const onChange = (newValue: string) => {
+    setSeparator(newValue);
+    GM.setValue('separator', newValue).catch(console.error);
+  };
+
   return (
     <ToolLine title="Keep single language">
       <div class="buttons">
-      <Button
-        class="button"
-        onClick={() => {
-          removeLHS().catch(console.error);
-        }}
-      >
-        Remove LHS
-      </Button>
-      <Button
-        class="button"
-        onClick={() => {
-          removeRHS().catch(console.error);
-        }}
-      >
-        Remove RHS
-      </Button>
-    </div>
+        <Button
+          class="button"
+          onClick={() => {
+            removeLHS(separator()).catch(console.error);
+          }}
+          disabled={separator().length == 0}
+        >
+          Remove LHS
+        </Button>
+        <Button
+          class="button"
+          onClick={() => {
+            removeRHS(separator()).catch(console.error);
+          }}
+          disabled={separator().length == 0}
+        >
+          Remove RHS
+        </Button>
+      </div>
+      <TextField value={separator()} onChange={onChange}>
+        <TextField.Label style={{padding: '6px'}}>separator:</TextField.Label>
+        <TextField.Input style={{width: '3em', 'text-align': 'center'}} />
+      </TextField>
     </ToolLine>
   );
 }
@@ -52,5 +66,7 @@ export async function createUI() {
 
   const container = (<div id={containerId}></div>) as HTMLDivElement;
   theToolbox.appendChild(container);
-  render(() => <SingleLanguageTracklistUI />, container);
+
+  const separator = await GM.getValue('separator', '=');
+  render(() => <SingleLanguageTracklistUI separator={separator} />, container);
 }
