d327a310aaaa9900b88294ae4c5c32d0a71d2a5f
dvirtz <dvirtz@gmail.com>
2026-04-19 22:20:09 +0100

    chore(acum-work-import): better type info type deduction

--- lib/musicbrainz-ext/src/type-info.ts
+++ lib/musicbrainz-ext/src/type-info.ts
@@ -2,24 +2,36 @@ import {tryFetchJSON} from '#fetch.ts';
 import PLazy from 'p-lazy';
 import {LinkedEntitiesT, OptionTreeT} from 'typedbrainz/types';
 
-function byId<T extends {id: number}>(list: ReadonlyArray<T>) {
+type TypeInfoMapKey = {
+  [K in keyof LinkedEntitiesT]: LinkedEntitiesT[K] extends Record<string | number, {id: number}> ? K : never;
+}[keyof LinkedEntitiesT];
+
+function byId<T extends {id: number}>(list: ReadonlyArray<T>): Record<number, T> {
   return Object.fromEntries(list.map(item => [item.id, item])) as Record<number, T>;
 }
 
-function fetchTypeInfo<T extends {id: number}>(url: string, key: string) {
+function fetchTypeInfo<T extends {id: number}>(url: string, key: string): PLazy<Record<number, T>> {
   return PLazy.from(async () => byId((await tryFetchJSON<{[key: string]: T[]}>(url))?.[key] ?? []));
 }
 
-function fetchOrGetFromCache<K extends keyof LinkedEntitiesT, T extends LinkedEntitiesT[K][0] & {id: number}>(
+function fetchTypeInfoMap<K extends TypeInfoMapKey>(url: string, key: string): PLazy<LinkedEntitiesT[K]> {
+  return PLazy.from(async () => {
+    const items = (await tryFetchJSON<{[key: string]: Array<LinkedEntitiesT[K][number]>}>(url))?.[key] ?? [];
+    return byId(items) as LinkedEntitiesT[K];
+  });
+}
+
+function fetchOrGetFromCache<K extends TypeInfoMapKey>(
   url: string,
   key: string,
   cacheKey?: K
-) {
-  return PLazy.from(async () =>
-    typeof MB !== 'undefined' && cacheKey && Object.keys(MB.linkedEntities[cacheKey]).length > 0
-      ? MB.linkedEntities[cacheKey]
-      : fetchTypeInfo<T>(url, key)
-  );
+): PLazy<LinkedEntitiesT[K]> {
+  return PLazy.from(async () => {
+    if (typeof MB !== 'undefined' && cacheKey && Object.keys(MB.linkedEntities[cacheKey]).length > 0) {
+      return MB.linkedEntities[cacheKey];
+    }
+    return await fetchTypeInfoMap<K>(url, key);
+  });
 }
 
 export const workAttributeTypes = fetchOrGetFromCache(
