8174bc1b68117fbdb59ea874b3c1ba380a2c45be
dvirtz <dvirtz@gmail.com>
2026-07-16 22:05:34 +0100

    fix(scaffold-festival-days): match only main page

--- scripts/scaffold-festival-days/vite.config.ts
+++ scripts/scaffold-festival-days/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig('scaffold-festival-days', {
   version: '1.1.1',
 
   match: ['http*://*.musicbrainz.org/event/*'],
-  'exclude-match': ['http*://*.musicbrainz.org/event/*/*', 'http*://*.musicbrainz.org/event/create'],
+  exclude: ['http*://*.musicbrainz.org/event/*/*'],
   'run-at': 'document-end',
 });
