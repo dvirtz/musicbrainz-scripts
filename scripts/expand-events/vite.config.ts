77f08149a39dbf2f67a5436f646047e50a52dde4
dvirtz <dvirtz@gmail.com>
2026-07-28 23:29:46 +0100

    feat(expand-events): expand event series

--- scripts/expand-events/vite.config.ts
+++ scripts/expand-events/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig('expand-events', {
     "See what's inside sub-events without having to follow their URL. Also adds convenient edit links for it.",
   version: '1.2.0-beta.2',
 
-  match: ['http*://*.musicbrainz.org/event/*'],
+  match: ['http*://*.musicbrainz.org/event/*', 'http*://*.musicbrainz.org/series/*'],
   'run-at': 'document-end',
 });
