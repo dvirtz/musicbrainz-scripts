b41af6706c0aac1ea8b6ef145e38c274e8ea2d3e
dvirtz <dvirtz@gmail.com>
2026-08-04 22:51:20 +0100

    feat(acum-work-import): update work object
    causes React to recreate the work link which requires WorkEditor to be recreated as well

--- scripts/acum-work-import/src/ui/work-edit-data-provider.tsx
+++ scripts/acum-work-import/src/ui/work-edit-data-provider.tsx
@@ -3,21 +3,38 @@ import {ArtistLookupCache} from '#artists.ts';
 import {linkArrangers, linkWriters} from '#link-artists.ts';
 import {PerWorkWarning} from '#ui/work-warnings.tsx';
 import {WorkEditData, workEditData, workEditDataEqual} from '#work-edit-data.ts';
-import {assertMBTree, assertReleaseRelationshipEditor} from '@repo/musicbrainz-ext/asserts';
+import {createWork} from '#works.ts';
+import {assertMBTree, assertRelationshipEditor, assertReleaseRelationshipEditor} from '@repo/musicbrainz-ext/asserts';
 import {buildOptionList, buildOptionListFromKeys} from '@repo/musicbrainz-ext/build-options-list';
 import {compareInsensitive, compareNumbers, compareWorks} from '@repo/musicbrainz-ext/compare';
 import {urlFromMbid} from '@repo/musicbrainz-ext/edits';
-import {findTargetTypeGroups} from '@repo/musicbrainz-ext/type-group';
+import {findTargetTypeGroups, iterateRelationshipsInTargetTypeGroup} from '@repo/musicbrainz-ext/type-group';
 import {WorkAttributeTypeAllowedValueT} from '@repo/musicbrainz-ext/type-info';
 import {createContext, createEffect, createResource, createSignal, ParentProps, useContext} from 'solid-js';
 import {createStore, reconcile, unwrap} from 'solid-js/store';
-import {LanguageT, RecordingT, WorkAttributeTypeT, WorkT, WorkTypeT} from 'typedbrainz/types';
+import {
+  IswcT,
+  LanguageT,
+  RecordingT,
+  WorkAttributeT,
+  WorkAttributeTypeT,
+  WorkLanguageT,
+  WorkT,
+  WorkTypeT,
+} from 'typedbrainz/types';
 
 type WorkTypeInfo = {
   workTypes: WorkTypeT[];
   workLanguages: LanguageT[];
   workAttributeTypes: WorkAttributeTypeT[];
   workAttributeAllowedValues: WorkAttributeTypeAllowedValueT[];
+};
+
+export type WorkEditDataInitialState = {
+  liveEditData: WorkEditData;
+  savedEditData: WorkEditData;
+  originalEditData: WorkEditData;
+  warnings: readonly PerWorkWarning[];
 };
 
 export type WorkEditDataProviderProps = ParentProps & {
@@ -27,6 +44,7 @@ export type WorkEditDataProviderProps = ParentProps & {
   recording?: RecordingT;
   typeInfo: WorkTypeInfo;
   shouldLinkArrangers: boolean;
+  initialState?: WorkEditDataInitialState;
 };
 
 function emptyEditData(): WorkEditData {
@@ -53,6 +71,75 @@ function sanitizeEditData(editData: WorkEditData): WorkEditData {
     iswcs: editData.iswcs.filter(iswc => iswc !== ''),
     attributes: editData.attributes.filter(attr => attr.value !== ''),
   };
+}
+
+function applyEditDataToWork(work: WorkT, editData: WorkEditData): WorkT {
+  // Create a new work object with the edited data
+  const newWork = createWork({
+    ...work,
+    name: editData.name,
+    comment: editData.comment,
+    typeID: editData.type_id,
+    languages: editData.languages.map(id => {
+      const lang = Object.values(MB?.linkedEntities.language ?? {}).find((l: LanguageT) => l.id === id);
+      return {
+        language: lang || ({id, entityType: 'language'} as LanguageT),
+        entityType: 'work-language',
+      } as WorkLanguageT;
+    }),
+    iswcs: editData.iswcs.map(
+      iswc =>
+        ({
+          iswc,
+          work_id: work.id,
+          entityType: 'iswc',
+        }) as IswcT
+    ),
+    attributes: editData.attributes.map(
+      attr =>
+        ({
+          typeID: attr.type_id,
+          value: attr.value,
+          entityType: 'work-attribute',
+        }) as unknown as WorkAttributeT
+    ),
+  });
+
+  // Dispatch update actions for all relationships pointing to this work
+  assertMBTree(MB?.tree);
+  assertRelationshipEditor(MB.relationshipEditor);
+
+  const targetTypeGroups = findTargetTypeGroups(MB.relationshipEditor.state.relationshipsBySource, work);
+
+  if (targetTypeGroups) {
+    for (const targetTypeGroup of MB.tree.iterate(targetTypeGroups)) {
+      for (const rel of iterateRelationshipsInTargetTypeGroup(targetTypeGroup)) {
+        if (rel.entity0.id === work.id || rel.entity1.id === work.id) {
+          // Clone the relationship and update entity references
+          const clonedRel = structuredClone(rel);
+          if (clonedRel.entity0.id === work.id) {
+            clonedRel.entity0 = newWork;
+          }
+          if (clonedRel.entity1.id === work.id) {
+            clonedRel.entity1 = newWork;
+          }
+
+          MB.relationshipEditor.dispatch?.({
+            type: 'update-relationship-state',
+            sourceEntity: clonedRel.entity0,
+            batchSelectionCount: undefined,
+            creditsToChangeForSource: '',
+            creditsToChangeForTarget: '',
+            newRelationshipState: clonedRel,
+            oldRelationshipState: rel,
+          });
+        }
+      }
+    }
+  }
+
+  MB.linkedEntities.work[newWork.id] = newWork;
+  return newWork;
 }
 
 function refreshRecordingState(recording: RecordingT) {
@@ -98,15 +185,18 @@ function makeWorkEditDataContext(
   workTypeInfo: WorkTypeInfo,
   refetch: () => void
 ) {
+  const [replacedWork, setReplacedWork] = createSignal<WorkT | undefined>(undefined);
+
   return {
     liveEditData,
     setLiveEditData,
     isModified: () => !workEditDataEqual(originalEditData(), savedEditData()),
-    workName: () => liveEditData.name,
     submitUrl: () => (work.gid ? urlFromMbid('work', work.gid) : '/work/create'),
     saveEditData: () => {
       const next = sanitizeEditData(unwrap(liveEditData));
       setSavedEditData(next);
+      const newWork = applyEditDataToWork(work, next);
+      setReplacedWork(newWork);
       setLiveEditData(reconcile(cloneEditData(next)));
     },
     restoreEditData: () => {
@@ -128,6 +218,13 @@ function makeWorkEditDataContext(
     warnings,
     isLoading,
     refetch,
+    replacedWork,
+    captureState: (): WorkEditDataInitialState => ({
+      liveEditData: structuredClone(unwrap(liveEditData)),
+      savedEditData: savedEditData(),
+      originalEditData: originalEditData(),
+      warnings: warnings(),
+    }),
   } as const;
 }
 
@@ -142,14 +239,17 @@ export function useWorkEditData() {
 }
 
 export function WorkEditDataProvider(props: WorkEditDataProviderProps) {
-  const [liveEditData, setLiveEditData] = createStore(emptyEditData());
-  const [savedEditData, setSavedEditData] = createSignal(emptyEditData());
-  const [originalEditData, setOriginalEditData] = createSignal(emptyEditData());
-  const [warnings, setWarnings] = createSignal<readonly PerWorkWarning[]>([]);
-  const [isLoading, setIsLoading] = createSignal(false);
+  const [liveEditData, setLiveEditData] = createStore(props.initialState?.liveEditData ?? emptyEditData());
+  const [savedEditData, setSavedEditData] = createSignal(props.initialState?.savedEditData ?? emptyEditData());
+  const [originalEditData, setOriginalEditData] = createSignal(props.initialState?.originalEditData ?? emptyEditData());
+  const [warnings, setWarnings] = createSignal<readonly PerWorkWarning[]>(props.initialState?.warnings ?? []);
+  const [isLoading, setIsLoading] = createSignal(!props.initialState);
 
   const [resource, {refetch}] = createResource(
-    [props.work, props.track, props.recording, props.artistCache, props.shouldLinkArrangers] as const,
+    () =>
+      props.initialState
+        ? null
+        : ([props.work, props.track, props.recording, props.artistCache, props.shouldLinkArrangers] as const),
     async ([work, track, recording, artistCache, shouldLinkArrangers]) => {
       setIsLoading(true);
       setWarnings([]);
